b3f91c79943e0357e73ffeb8ebbfc5d4eabd6786
Dandy <dandy.chen@advantech.com.tw>
2018-07-16 14:16:53 +0800

    add .vscode files
    1. add .vscode
    
    2.remove .vscode from ignore list

---
+++ .vscode/blueprint-templates/vue-component-template/__pascalCase_name__.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="{{ camelCase name }}">
+  </div>
+</template>
+
+<script>
+export default {
+  name: '{{ camelCase name }}',
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  watch: {
+    // test(new, old) {}
+  },
+  methods: {
+    test() {},
+  },
+};
+</script>
