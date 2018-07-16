01365b775566994b1b1a1be0fd0260b3505a54fd
Dandy <dandy.chen@advantech.com.tw>
2018-07-16 17:20:30 +0800

    finish login layout

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import './assets/stylesheet/build/all.min.css';
 import App from './App';
 import router from './router';
 
---
+++ src/pages/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="login">
+    <div class="wrap container">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="title">Login</h1>
+        </div>
+        <div class="col-12 login_form">
+          <ol>
+            <li class="form_item">
+              <h2>Account</h2>
+              <b-form-input v-model.trim="login.account"
+                            type="text"
+                            placeholder="Enter your account"></b-form-input>
+            </li>
+            <li class="form_item">
+              <h2>Password</h2>
+              <b-form-input v-model.trim="login.password"
+                            type="text"
+                            placeholder="Enter your password"></b-form-input>
+            </li>
+          </ol>
+          <button type="button"
+                  class="btn btn-primary form_btn w-100"
+                  :disabled="isSubmitDisabled">Sign in</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  components: {},
+  data() {
+    return {
+      login: {
+        account: '',
+        password: '',
+      },
+    };
+  },
+  created() {},
+  mounted() {},
+  computed: {
+    isSubmitDisabled() {
+      return this.login.account === '' || this.login.password === '';
+    },
+  },
+  watch: {
+    // var(new, old) {}
+  },
+  methods: {
+    // foo() {},
+  },
+};
+</script>
