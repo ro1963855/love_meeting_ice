78a7a487e3b3f895717bbfdc30aa1826e4985d08
Dandy <dandy.chen@advantech.com.tw>
2018-07-16 14:40:51 +0800

    add fontawesome-font module
    1.cmd
    npm i --save @fortawesome/fontawesome-svg-core
    npm i --save @fortawesome/free-solid-svg-icons
    npm i --save @fortawesome/free-regular-svg-icons
    npm i --save @fortawesome/free-brand-svg-icons
    npm i --save @fortawesome/free-brands-svg-icons
    
    2.setting src/main.js

--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import App from './App';
 import router from './router';
 
+library.add(fas);
+library.add(far);
+library.add(fab);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
