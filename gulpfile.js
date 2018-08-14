ef9e96381b9918aa19a48d429deea0ddfb9dab44
Dandy <dandy.chen@advantech.com.tw>
2018-08-14 11:09:19 +0800

    add gulp compile typescript filem, gulify and minify javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,68 @@
-// Sass configuration
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const minifyCSS = require('gulp-minify-css');
+const rename = require('gulp-rename');
+const ts = require('gulp-typescript');
+const uglify = require('gulp-uglify');
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var minifyCSS = require('gulp-minify-css');
-var rename = require("gulp-rename");
+const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('sass', function () {
-    gulp.src('./src/assets/stylesheet/scss/**/*.scss')
-        .pipe(sass())
-        .on('error', console.error.bind(console))
-        .pipe(gulp.dest('./src/assets/stylesheet/css/'))
+gulp.task('sass', () => {
+  gulp
+    .src('./src/assets/stylesheet/scss/**/*.scss')
+    .pipe(sass())
+    .on('error', console.error.bind(console))
+    .pipe(gulp.dest('./src/assets/stylesheet/css/'));
 });
 
-gulp.task('concat', function () {
-    return gulp.src('./src/assets/stylesheet/css/**/*.css')
-        .pipe(concat('all.css'))
-        .pipe(gulp.dest('./src/assets/stylesheet/build/'));
+gulp.task('concat', () => {
+  return gulp
+    .src('./src/assets/stylesheet/css/**/*.css')
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest('./src/assets/stylesheet/build/'));
 });
 
-gulp.task('minify-css', ['concat'], function () {
-    return gulp.src('./src/assets/stylesheet/build/all.css')
-        .pipe(minifyCSS({
-            keepBreaks: true,
-        }))
-        .pipe(rename(function (path) {
-            path.basename += ".min";
-            path.extname = ".css";
-        }))
-        .pipe(gulp.dest('./src/assets/stylesheet/build/'));
+gulp.task('minify-css', ['concat'], () => {
+  return gulp
+    .src('./src/assets/stylesheet/build/all.css')
+    .pipe(
+      minifyCSS({
+        keepBreaks: true,
+      }),
+    )
+    .pipe(
+      rename((path) => {
+        path.basename += '.min';
+        path.extname = '.css';
+      }),
+    )
+    .pipe(gulp.dest('./src/assets/stylesheet/build/'));
 });
 
-gulp.task('default', ['sass', 'minify-css'], function () {
-    gulp.watch('./src/assets/stylesheet/scss/**/*.scss', ['sass']);
-    gulp.watch('./src/assets/stylesheet/css/**/*.css', ['minify-css']);
+gulp.task('typescript', () => {
+  return gulp
+    .src('./src/assets/javascript/ts/**/*.ts')
+    .pipe(tsProject())
+    .js.pipe(gulp.dest('./src/assets/javascript/js/'));
+});
+
+gulp.task('uglify', () => {
+  return gulp
+    .src('./src/assets/javascript/js/**/*.js')
+    .pipe(uglify())
+    .pipe(
+      rename((path) => {
+        path.basename += '.min';
+        path.extname = '.js';
+      }),
+    )
+    .pipe(gulp.dest('./src/assets/javascript/build/'));
+});
+
+gulp.task('default', ['sass', 'minify-css', 'typescript', 'uglify'], () => {
+  gulp.watch('./src/assets/stylesheet/scss/**/*.scss', ['sass']);
+  gulp.watch('./src/assets/stylesheet/css/**/*.css', ['minify-css']);
+  gulp.watch('./src/assets/javascript/ts/**/*.ts', ['typescript']);
+  gulp.watch('./src/assets/javascript/js/**/*.js', ['uglify']);
 });
